8c9ef9a1de7e6a94d72201973928fd81db65e991
wjdwndtlr <wjdwndtlr1024@gmail.com>
2019-12-27 00:09:41 +0900

    Updates

---
+++ precache-manifest.7a61a6dbaf30b97fbacce8047c2d6d48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b6056d06e33d2692bdf0266c3484a20",
+    "url": "/youflix/index.html"
+  },
+  {
+    "revision": "01c83d9f04318930898d",
+    "url": "/youflix/static/js/2.5b91750f.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/youflix/static/js/2.5b91750f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b1e69227d3b795479e6",
+    "url": "/youflix/static/js/main.401671f2.chunk.js"
+  },
+  {
+    "revision": "8014142851349bea6a65",
+    "url": "/youflix/static/js/runtime-main.4a19b027.js"
+  },
+  {
+    "revision": "59373d1f3e32cfbd0cefe58ef38d4ec6",
+    "url": "/youflix/static/media/imDb.59373d1f.png"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/youflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
